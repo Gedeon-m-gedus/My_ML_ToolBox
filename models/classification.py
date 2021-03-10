f033fc0823084d4a0555156c2e03c5593b5a90ce
gmuhawen <gedeon.muhawenayo@inria.fr>
2021-03-10 16:01:31 +0200

    update

--- models/classification.py
+++ models/classification.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class perceptron:
     def __init__(self, learning_rate=0.1, n_iter=500):
         self.lr = learning_rate
@@ -10,7 +12,6 @@ class perceptron:
         y = np.where(y==0,-1,1)
         print('Number training of samples: ', N, '\nNumber of features: ',d)
         self.parameters = np.zeros(d)
-        print(self.parameters)
         for count in range(self.n_iter):
             for sample_index in range(N):
                 X_i = X[sample_index]
@@ -19,17 +20,21 @@ class perceptron:
                     pass
                 else:
                     self.parameters = self.parameters - self.lr * X_i
-   
         print(self.parameters)
+
     
     def predict(self, data):
         preds = np.sign(data.dot(self.parameters))
         preds = np.where(preds==-1,0,1)
         return preds
-        print('This function will predict on the given data') 
+
     
     def accuracy(self, lebel, pred):
         acc = 0
         for i, _ in enumerate(lebel):
             if pred[i] == lebel[i]:acc+=1
         return acc/len(pred)
+
+
+class logisticRegression:
+	pass
