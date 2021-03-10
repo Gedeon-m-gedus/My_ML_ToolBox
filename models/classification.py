3ba240501ca284756dac79a2ed5bbb9687215b33
gmuhawen <gedeon.muhawenayo@inria.fr>
2021-03-10 16:25:16 +0200

    update

--- models/classification.py
+++ models/classification.py
@@ -7,26 +7,36 @@ class perceptron:
         self.parameters = None
         
     def fit(self, X, y):
+	b = np.ones(X.shape[0])
+        b = b.reshape(b.shape[0],-1)
+        X = X.reshape(X.shape[0],-1)
+        X = np.hstack((b,X))
+        y = np.where(y==0,-1,1)
+
         N = X.shape[0]
         d = X.shape[1]
-        y = np.where(y==0,-1,1)
+        
         print('Number training of samples: ', N, '\nNumber of features: ',d)
-        self.parameters = np.zeros(d)
+
+        self.parameters = np.random.rand(d)
         for count in range(self.n_iter):
             for sample_index in range(N):
                 X_i = X[sample_index]
-                pred = np.sign(X_i.dot(self.parameters))
+                pred = np.sign(self.parameters.T.dot(X_i))
                 if pred == y[sample_index]:
                     pass
                 else:
-                    self.parameters = self.parameters - self.lr * X_i
+                    self.parameters = self.parameters - self.lr * (y[sample_index] - pred) * X_i
         print(self.parameters)
 
     
-    def predict(self, data):
-        preds = np.sign(data.dot(self.parameters))
-        preds = np.where(preds==-1,0,1)
-        return preds
+    def predict(self, X):
+        b = np.ones(X.shape[0])
+        b = b.reshape(b.shape[0],-1)
+        X = X.reshape(X.shape[0],-1)
+        X = np.hstack((b,X))
+        pred = np.sign(X.dot(self.parameters))
+        return np.where(pred==1,1,0)
 
     
     def accuracy(self, lebel, pred):
